431ac9d584594fd774a08eb20cbb752424a0910f
vinnyasa <vinnyasa@gmail.com>
2018-11-04 14:05:11 -0800

    Added routes to app.js

--- app.js
+++ app.js
@@ -1,41 +1,188 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var mysql = require('./dbcon.js');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(express.static('public'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 62333);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/',function(req,res){
+
+    var title = "Workout Tracker";
+
+    var formTitle = "Exercise Form";
+    var tableTitle = "Exercise Records";
+
+    var context = {};
+    context.title = title;
+    context.formTitle = formTitle;
+    context.tableTitle = tableTitle;
+
+    // send table
+
+
+    res.render('home', context);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+    mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+        var createString = "CREATE TABLE workouts("+
+            "id INT PRIMARY KEY AUTO_INCREMENT,"+
+            "name VARCHAR(255) NOT NULL,"+
+            "reps INT,"+
+            "weight INT,"+
+            "date DATE,"+
+            "lbs BOOLEAN)";
+        mysql.pool.query(createString, function(err){
+            context.results = "Table reset";
+            res.render('home',context);
+        })
+    });
 });
+
+
+app.get('/load', function (req, res) {
+    console.log("server side load");
+    mysql.pool.query('SELECT * FROM workouts ORDER BY id', function(err, rows, fields) {
+        if (err) {
+
+            next(err);
+            return;
+        }
+        //console.log(rows);
+        res.send(rows);
+    });
+});
+
+app.get('/insert', function(req,res){
+    var context = {};
+
+    console.log("server side post, ready to process mysql");
+
+    mysql.pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)", [req.headers.name, req.headers.reps, req.headers.weight, req.headers.day,  req.headers.lbs], function(err, result) {
+        if (err) {
+            next(err);
+            return;
+        }
+
+        context.id = result.insertId;
+        context.status = 201;
+    });
+
+    //get row
+    mysql.pool.query('SELECT * FROM workouts ORDER BY id DESC LIMIT 1', function(err, rows, fields){
+        if(err){
+
+            next(err);
+            return;
+        }
+
+        //send the information back to client
+        context.response = rows[rows.length-1];
+
+        res.send(context);
+
+    });
+
+});
+
+
+app.get('/delete', function(req,res) {
+    var context = {};
+
+    console.log("server side delete, ready to delete from mysql");
+
+    mysql.pool.query("DELETE FROM workouts WHERE id=?", [req.headers.id], function(err, result){
+        if(err){
+            next(err);
+            return;
+        }
+        res.send(result);
+    });
+});
+
+app.post('/edit', function(req,res,next){
+    console.log("server side preparing edit page");
+    console.log(req.body);
+    var context = {};
+
+    mysql.pool.query('SELECT * FROM workouts WHERE id =?',[req.body.id],function(err, rows, fields){
+        if(err){
+            next(err);
+            return;
+        }
+
+        context.response = rows[rows.length-1];
+
+        var date = JSON.stringify(context.response.date);
+
+        context.date = date;
+        res.render('edit', context);
+    });
+});
+
+/* for testing only */
+app.post('/edit-form', function (req, res) {
+    var context = {};
+    context.title = "Edit Form";
+    context.name = "";
+    context.id = req.body.id;
+    context.reps = "";
+    context.weight = "";
+    context.date = "2018-07-07";
+    context.lbs = 0;
+
+    res.render('edit', context);
+});
+
+/* process update of table and sql after edit form */
+app.post('/update', function (req, res) {
+    console.log("server side update data ready for mysql");
+    var lbs = 1;
+    if (req.body.unit !== "lbs") {
+        lbs = 0;
+    }
+
+    mysql.pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?",
+        [req.body.name, req.body.reps, req.body.weight, req.body.date, lbs, req.body.id],
+        function(err, result){
+            if(err){
+                next(err);
+                return;
+            }
+            console.log("Row updated. redirecting to home");
+            res.redirect('/');
+        });
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
+
+
+
+
+
 
 module.exports = app;
